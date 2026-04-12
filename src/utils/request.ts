c59242e289ab0d314a66b127594d1836ab1839fb
doujh <doujh@163.com>
2026-04-12 11:35:07 +0800

    修改环境参数

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,13 @@
 import { getDeviceId } from './device'
 
-const BASE_URL = 'http://192.168.2.99:9199'
+//const BASE_URL = 'http://192.168.2.48:9199'       // AI-48 48开发环境
+//const BASE_URL = 'http://192.168.2.97:9199'       // AI-97 97前端开发环境
+//const BASE_URL = 'http://192.168.2.98:9199'       // AI-98 bug开发环境
+//const BASE_URL = 'http://192.168.2.99:9199'         // AI-99 dev开发环境
+config.baseUrl = 'http://192.168.2.103:9199'      //测AI-103 测试环境
+//config.baseUrl = "https://staging.jianbiyou.com/gateway"; //预发布环境接口
+// config.baseUrl = "https://pre.jianbiyou.com/gateway"; //影子环境接口
+// config.baseUrl = "https://cos.jianbiyou.com/gateway"; //生产环境接口
 
 // 统一响应结构
 export interface ApiResponse<T = unknown> {
