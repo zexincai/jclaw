cf6d6c0a914638fff0e6a9e265edd84b0405fc3b
caizexin <zexincaicai@163.com>
2026-04-30 15:15:30 +0800

    fix: 更新 WebSocket 连接地址

--- src/composables/useWukongIM.ts
+++ src/composables/useWukongIM.ts
@@ -71,7 +71,7 @@ export function useWukongIM() {
           callback(wsAddr)
         }
       } else {
-        WKSDK.shared().config.addr = 'ws://100.112.82.63:5200'
+        WKSDK.shared().config.addr = wsAddr
       }
       currentTelephone = telephone
       WKSDK.shared().config.uid = userId
